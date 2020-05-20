0b8328042501ed3738e379ae340fd48c756a2dab
miguelsm <miguelsantesmases@gmail.com>
2020-05-20 23:42:22 +0100

    Configure web-mode indetation settings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -172,6 +172,18 @@
 (global-undo-tree-mode)
 
 ;;
+;; web-mode
+;;
+
+(defun my-web-mode-hook ()
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-json-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;;
 ;; UI
 ;;
 
